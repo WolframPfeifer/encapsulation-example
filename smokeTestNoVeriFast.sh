78e566ba7101f0d403bd54c7132de1fe3e7aa81f
Wolfram Pfeifer <wolfram.pfeifer@kit.edu>
2026-02-12 17:43:40 +0100

    include citool in smoke test

--- smokeTestNoVeriFast.sh
+++ smokeTestNoVeriFast.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# check that both shipped KeY versions are executable
-echo "Checking that KeY (standard Dynamic Frames variant) is executable (should print a list of all set properties) ..."
-java -jar tools/key-2.12.4-dev-exe.jar "--show-properties"
+echo "Checking that KeY (standard Dynamic Frames variant) is executable (should print help message of KeY) ..."
+java -Dslf4j.internal.verbosity=ERROR -Dlogback.configurationFile=tools/disablelogging.xml -jar tools/key-2.12.4-dev-exe.jar "--help"
 echo
 echo "Checking that the Universe Encapsulation Type checker is executable (should print the help message of the Java compiler) ..."
 tools/uet-checker/checkEnc.sh --help
 echo
-echo "Checking that KeY (Universe Encapsulation Types variant) is executable (should print a list of the available flags and parameters) ..."
+echo "Checking that KeY (Universe Encapsulation Types variant) is executable (should print help message of KeY) ..."
 java -Dslf4j.internal.verbosity=ERROR -Dlogback.configurationFile=tools/disablelogging.xml -jar tools/key-2.12.4-UT-dev-exe.jar "--help"
 echo
+echo "Checking that citool is executable (should print help message of citool) ..."
+java -cp tools/key-2.12.4-dev-exe.jar:tools/citool-1.7.0-SNAPSHOT-mini.jar io.github.wadoon.keycitool.CheckerKt --help

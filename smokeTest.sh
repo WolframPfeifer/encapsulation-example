2dbbc9be6fe584562304fd3d88da898064e1a50b
Mattias Ulbrich <ulbrich@kit.edu>
2026-02-12 20:30:12 +0100

    making the script output more convincing
    smoke: different commands
    check: more visual guidance

--- smokeTest.sh
+++ smokeTest.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # check that VeriFast is executable
-echo "Checking that VeriFast is executable (should print a list of available flags and parameters) ..."
+echo "################################################################################"
+echo "## Ensuring that VeriFast is executable"
+echo "##"
+echo "## This should print the version 26.01 of verifast and the beginning of the help message"
+echo "## The full output is stored in verifast.out"
+echo "################################################################################"
 # should print a list of the available flags and parameters of VeriFast
-./tools/vf/verifast-26.01-linux-amd64/bin/verifast --help
-echo
+./tools/vf/verifast-26.01-linux-amd64/bin/verifast --help 2>&1 | tee verifast.out | grep -v "^  -"
 
+echo
 # check that other tools are executable
 ./smokeTestNoVeriFast.sh
